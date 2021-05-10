78a9fe44f3b98b0cb307be394a7d93b8c72f27a6
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2021-05-10 08:57:45 -0400

    adjusting mongoUrl

--- src/config/index.js
+++ src/config/index.js
@@ -9,7 +9,7 @@ if(version == 'development'){
     mongoUrl = 'mongodb://localhost:27017/dev-todo-tasks'
 }
 else if(version == 'production'){
-    mongoUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.u75bl.mongodb.net/todo-task`
+    mongoUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.u75bl.mongodb.net/todo-task?retryWrites=true&w=majority`
 }
 
 module.exports = {
